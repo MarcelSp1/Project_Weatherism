50e179eea3565350dba248cbc5741da9e3535359
MarcelSp1 <spillemarcel@gmail.com>
2024-12-16 19:07:31 +0100

    deleted venv and some bugfixes in read_sensor1.py

--- read_sensor1.py
+++ read_sensor1.py
@@ -4,11 +4,19 @@ import adafruit_dht
 import csv
 from datetime import datetime
 
-# Funktion um den Sensor auszulesen
-def get_temp_hmd():
+def initialize_device():
     #Den DHT22 initiieren
-    dhtDevice = adafruit_dht.DHT22(board.D2)
+    try:
+        device_port = adafruit_dht.DHT22(board.D2) # D2 gibt den GPiO Pin an
+    except Exception as e:
+        print(f("Der DHT22 konnte nicht korrekt initialisiert werden. Es gab folgenden Fehler: {e}"))
 
+    return device_port
+
+
+# Funktion um den Sensor auszulesen
+def get_temp_hmd(device_port):
+    dhtDevice = device_port
     # Lese den Sensor so lange aus, bis die Daten abgerufen werden können
     while True:
         try:
@@ -20,12 +28,20 @@ def get_temp_hmd():
 
         except RuntimeError as error:
             # Wenn Error: einfach nochmal versuchen (-> schwieriges Auslesen)
-            print(error.args[0])
+            date = datetime.now()
+            extracted_date = date.date()
+            extracted_time = date.time()
+            print(extracted_date, " ", extracted_time, " Es gab einen erwarteten Fehler: ", error.args[0])
             time.sleep(2.0)
             continue
         except Exception as error:
+            date = datetime.now()
+            extracted_date = date.date()
+            extracted_time = date.time()
+            print(extracted_date, " ", extracted_time, " Es gab einen unerwarteten Fehler und der Sensor wird verlassen.")
             dhtDevice.exit()
             raise error
+
 
 # Notiere das Datum, die Zeit, die Luftfeuchtigkeit und die Temperatur in csv_data/humidity_and_temperature.csv
 def note_data(temp, hmd):
@@ -34,20 +50,30 @@ def note_data(temp, hmd):
     extracted_date = date.date()
     time = date.time()
     data = [[extracted_date, time, humidity, temperature]]
-    with open("csv_data/humidity_and_temperature.csv", mode="w", newline="", encoding="utf-8") as file:
+    with open("csv_data/humidity_and_temperature.csv", mode="a", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerows(data)
 
+
 # Notiere alle zwei Minuten die dafür ausgelesenen Daten
 def main_cycle():
+    device_port = initialize_device()
+    
     while True:
+        if device_port is None:
+            initialize_device()
+        
+        # Einholen des Datums und Extrahierung der Minutenangabe
         prep_time = datetime.now()
         prep_time = prep_time.time()
         prep_time = str(prep_time)
         prep_time = prep_time.split(":")
         time_minute = int(prep_time[1])
+        # Ausführung wenn es sich um eine gerade Minute handelt => Ausführung alle zwei Minuten
         if (time_minute % 2 == 0):
-            temp, hmd = get_temp_hmd()
+            temp, hmd = get_temp_hmd(device_port)
             note_data(temp, hmd)
+            time.sleep(60.0)
+
 
 main_cycle()
