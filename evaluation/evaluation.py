672e7b967233caa85ee4e0410fcac8fe020aa03e
derohneplan <gamingschrott70@gmail.com>
2025-01-05 21:15:56 +0100

    Now calculates hourly data

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from collections import defaultdict
 import os
+import csv
 
 # Datei laden
 data = pd.read_csv('./csv_data/humidity_and_temperature.csv')
@@ -21,30 +22,27 @@ for _, row in data.iterrows():
     result[date][hour]['humidity'].append(row['humidity'])
     result[date][hour]['temperature'].append(row['temperature'])
 
-# Prüfen, ob genügend Daten vorhanden sind und Fehler ausgeben, falls nicht im Error log angeben
-errors = []
-log_file = './evaluation/errors.log'
-os.makedirs(os.path.dirname(log_file), exist_ok=True)
-with open(log_file, 'w', encoding='utf-8') as log:
+def error_count():
+    errors = []
+    log_file = './evaluation/errors.log'
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    with open(log_file, 'w', encoding='utf-8') as log:
+        for date, hours in result.items():
+            for hour, values in hours.items():
+                if len(values['humidity']) < 30:
+                    error_message = f"Nicht genug Daten für {date} um {hour} Uhr. Nur {len(values['humidity'])} Daten gefunden.\n"
+                    errors.append(error_message)
+                    log.write(error_message)
+
+data = []
+hourly_data = './csv_data/hourly_data.csv'
+with open(hourly_data, 'w', encoding='utf-8') as csv:
     for date, hours in result.items():
         for hour, values in hours.items():
-            if len(values['humidity']) < 30:
-                error_message = f"Nicht genug Daten für {date} um {hour} Uhr. Nur {len(values['humidity'])} Daten gefunden.\n"
-                errors.append(error_message)
-                log.write(error_message)
-    
-
-#------------------------ Ab hier Löschen
-# # Ergebnis anzeigen (als Beispiel für ein Datum)
-example_date = next(iter(result))
-example_output = {example_date: result[example_date]}
-print("Example output:", example_output)
-
-# Speichern in einer Datei für weitere Verwendung
-import json
-output_file = './evaluation/grouped_data.json'
-with open(output_file, 'w') as f:
-    json.dump({str(date): {hour: dict(data) for hour, data in hours.items()} for date, hours in result.items()}, f)
-
-print(f"Grouped data saved to {output_file}")
+            error_count()
+            values['average_humidity'] = sum(values['humidity']) / len(values['humidity'])
+            values['average_temperature'] = sum(values['temperature']) / len(values['temperature'])
+            hourly= f"{date}, {hour}:00 {values['average_temperature']};{values['average_humidity']}\n"
+            data.append(hourly)
+            csv.write(hourly)
 
