6a3d784e244fa7caa1e7fcbc13076e4bb20dba33
derohneplan <gamingschrott70@gmail.com>
2025-01-08 15:17:33 +0100

    file system change added to code and commented some lines

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -6,7 +6,7 @@ import csv
 print("Vorgang wird gestartet.")
 
 # Datei laden
-data = pd.read_csv('raw_data/modified_humidity_and_temperature.csv')
+data = pd.read_csv('evaluation/raw_data/modified_humidity_and_temperature.csv')
 
 # Datum und Uhrzeit zusammenfassen in eine Zeile
 data['datetime'] = pd.to_datetime(data['date'] + ' ' + data['time'])
@@ -23,8 +23,9 @@ for _, row in data.iterrows():
     result[date][hour]['humidity'].append(row['humidity'])
     result[date][hour]['temperature'].append(row['temperature'])
 
+#Werte pro Stunde zählen, falls keine 30 vorhanden sind, wird dies im Results Ordner im Error log vermerkt.
 def error_count():
-    log_file = 'results/errors.log'
+    log_file = 'evaluation/results/errors.log'
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
     with open(log_file, 'w', encoding='utf-8') as log:
         for date, hours in result.items():
@@ -33,8 +34,8 @@ def error_count():
                     error_message = f"Nicht genug Daten für {date} um {hour} Uhr. Nur {len(values['humidity'])} Daten gefunden.\n"
                     log.write(error_message)
 
-data = []
-hourly_data = 'results/hourly_data.csv'
+#Durschnittswerte pro Stunde errechnen und diese zur weiterverarbeitung notieren.
+hourly_data = 'evaluation/results/hourly_data.csv'
 with open(hourly_data, 'w', encoding='utf-8') as csv:
     for date, hours in result.items():
         for hour, values in hours.items():
@@ -43,5 +44,8 @@ with open(hourly_data, 'w', encoding='utf-8') as csv:
             values['average_temperature'] = sum(values['temperature']) / len(values['temperature'])
             hourly= f"{date}, {hour}:00 {values['average_temperature']};{values['average_humidity']}\n"
             csv.write(hourly)
+
+
+
 
 print("Vorgang wurde erfolgreich abgeschlossen.")
