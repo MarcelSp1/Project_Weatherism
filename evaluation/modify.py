4b156029a9f595c9ad56d4fabc40e7dcd786c12f
MarcelSp1 <spillemarcel@gmail.com>
2025-01-13 15:05:47 +0100

    added modifiy.py and original data

--- evaluation/modify.py
+++ evaluation/modify.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 import pandas as pd
 
-def copy_csv():
-    df = pd.read_csv('csv_data/humidity_and_temperature.csv')
-    df.to_csv('evaluation/testing/modified_humidity_and_temperature.csv')
 
-def main():
+def modify_data():
     while True:
         val = input("Wie viele Minuten sollen zu allen Daten hinzugerechnet werden? ")
         try:
@@ -14,11 +11,15 @@ def main():
         except ValueError:
             print("Bitte geben Sie eine ganzahlige Zahl ein.")
     print("Vorgang wird gestartet")
-    copy_csv()
-    df_mf = pd.read_csv('evaluation/testing/modified_humidity_and_temperature.csv')
-    temp_date = pd.to_datetime(df_mf['date'] + ' ' + df_mf['time'])
-    print(temp_date)
-    temp_date = temp_date + pd.Timedelta(minutes = val)
-    print(temp_date)
+    df = pd.read_csv('csv_data/rainfall2.csv')
+    df.to_csv('evaluation/testing/modified_rainfall2.csv', index=False)
+    df_mf = pd.read_csv('evaluation/testing/modified_rainfall2.csv')
+    temp_datetime = pd.to_datetime(df_mf['date'] + ' ' + df_mf['time'])
+    temp_datetime = temp_datetime + pd.Timedelta(minutes = val)
+    df_mf['date'] = temp_datetime.dt.date
+    df_mf['time'] = temp_datetime.dt.time
+    df_mf.to_csv('evaluation/raw_data/modified_rainfall2.csv', index=False)
+    print("Vorgang erfolgreich abgeschlossen.")
 
-main()
+
+modify_data()
