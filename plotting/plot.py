4363d57dae225143af9df899747630a886ca476b
MarcelSp1 <spillemarcel@gmail.com>
2025-01-20 22:03:13 +0100

    added documentation in plot.py

--- plotting/plot.py
+++ plotting/plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+# Erstellt Diagramme für Vergleich zwischen Temperatur und zugehöriger Vorhersage (12 Mal, jeweils für die Vorhersage n Stunden davor)
 def plot_temp(df_temp_hmd, df_forecast):
     colors = [
     '#003f5c',  # Dunkelblau
@@ -18,27 +19,40 @@ def plot_temp(df_temp_hmd, df_forecast):
     '#ff1493']  # Tiefrosa
 
     for i in range(12):
+        # Definiere Parameter für die Diagramme
         plt.figure()
         plt.xlabel('Datum')
         plt.ylabel('Temperatur in °C')
+
+        # Plotte die eigens erfassten Daten in das Diagramm
         plt.plot(df_temp_hmd['datetime'], df_temp_hmd['temperature'], marker='o', linestyle='-', ms=4, label='Eigens erfasste Temperaturdaten')
 
+        # Bereite den Dataframe für die zugehörige Vorhersage vor
         f_df_forecast = pd.DataFrame(columns=["date", "temperature"])
         f_df_forecast['date'] = pd.to_datetime(df_forecast['Date & Time'])
         
         time = 12 - i
         colors_now = colors[i - 1]
-        f_df_forecast['temperature'] = df_forecast[f'{time} Hours before'].str.split().str.get(0).astype('float64')
-        plt.title(f'Temperaturverlauf/Vorhersagenälte: {time} Stunden')
-        plt.plot(f_df_forecast['date'], f_df_forecast['temperature'], marker='o', color = colors_now, linestyle = '', ms=4, label='Vorhersage für die Temperatur')
-        ax = plt.gca()  # Aktuelle Achse abrufen
-        plt.xticks(rotation=45)  # Schrägstellen zur besseren Lesbarkeit
 
+        # Packe die jeweilige Spalte aus evaluation\results\sorted_weather_data.csv für die Vorhersage in den vorbereiteten Dataframe
+        f_df_forecast['temperature'] = df_forecast[f'{time} Hours before'].str.split().str.get(0).astype('float64')
+
+        # Gebe jedem Diagramm jeweils den passenden Titel
+        plt.title(f'Temperaturverlauf/Vorhersagenälte: {time} Stunden')
+
+        # Plotte die Vorhersagedaten
+        plt.plot(f_df_forecast['date'], f_df_forecast['temperature'], marker='o', color = colors_now, linestyle = '', ms=4, label='Vorhersage für die Temperatur')
+
+        # Schrägstellen der Datumsangaben auf der x-Achse zur besseren Lesbarkeit
+        plt.xticks(rotation=45)
+
+        # Erstelle die zugehörige Legende, speichere das Diagramm und schließe es, um Ressourcen zu sparen
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='Legende')
         plt.savefig(f'plotting/plot_results/temp_forecast/temp_forecast_{time}_hours_before', bbox_inches='tight')
         plt.close()
 
 
+# Erstellt Diagramme für Vergleich zwischen Luftfeuchtigkeit und zugehöriger Vorhersage (12 Mal, jeweils für die Vorhersage n Stunden davor)
 def plot_hmd(df_temp_hmd, df_forecast):
     colors = [
     '#003f5c',  # Dunkelblau
@@ -55,22 +69,34 @@ def plot_hmd(df_temp_hmd, df_forecast):
     '#ff1493']  # Tiefrosa
 
     for i in range(12):
+        # Definiere Parameter für die Diagramme
         plt.figure()
         plt.xlabel('Datum')
         plt.ylabel('Luftfeuchtigkeit in %')
+
+        # Plotte die eigens erfassten Daten in das Diagramm
         plt.plot(df_temp_hmd['datetime'], df_temp_hmd['humidity'], marker='o', linestyle='-', ms=4, label='Eigens erfasste Luftfeuchtigkeitswerte')
 
+        # Bereite den Dataframe für die zugehörige Vorhersage vor
         f_df_forecast = pd.DataFrame(columns=["date", "humidity"])
         f_df_forecast['date'] = pd.to_datetime(df_forecast['Date & Time'])
         
         time = 12 - i
         colors_now = colors[i - 1]
+
+        # Packe die jeweilige Spalte aus evaluation\results\sorted_weather_data.csv für die Vorhersage in den vorbereiteten Dataframe
         f_df_forecast['humidity'] = df_forecast[f'{time} Hours before'].str.split().str.get(1).astype('float64')
+
+        # Gebe jedem Diagramm jeweils den passenden Titel
         plt.title(f'Luftfeuchtigkeitsverlauf/Vorhersagenälte: {time} Stunden')
+
+        # Plotte die Vorhersagedaten
         plt.plot(f_df_forecast['date'], f_df_forecast['humidity'], marker='o', color = colors_now, linestyle = '', ms=4, label='Vorhersage für die Luftfeuchtigkeit')
-        ax = plt.gca()  # Aktuelle Achse abrufen
+
+        # Schrägstellen der Datumsangaben auf der x-Achse zur besseren Lesbarkeit
         plt.xticks(rotation=45)  # Schrägstellen zur besseren Lesbarkeit
 
+        # Erstelle die zugehörige Legende, speichere das Diagramm und schließe es, um Ressourcen zu sparen
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='Legende')
         plt.savefig(f'plotting/plot_results/hmd_forecast/hmd_forecast_{time}_hours_before', bbox_inches='tight')
         plt.close()
@@ -78,11 +104,14 @@ def plot_hmd(df_temp_hmd, df_forecast):
 def main():
     print('Starting...')
 
+    # Einlesen der erhobenen Daten und Datumsformatangleichung
     df_temp_hmd  = pd.read_csv('evaluation/raw_data/modified_humidity_and_temperature.csv')
-    df_temp_hmd['datetime'] = pd.to_datetime(df_temp_hmd['date'] + ' ' + df_temp_hmd['time'])#.dt.strftime('%Y-%m-%d %H:%M:%S')
+    df_temp_hmd['datetime'] = pd.to_datetime(df_temp_hmd['date'] + ' ' + df_temp_hmd['time'])
 
+    # Einlesen der sortierten Vorhersage
     df_forecast = pd.read_csv('evaluation/results/sorted_weather_data.csv')
 
+    # Erstelle Diagramme aus den eingelesenen Daten
     plot_hmd(df_temp_hmd, df_forecast)
     plot_temp(df_temp_hmd, df_forecast)
     print('Done.')
